dd0ebb73aa09d9b2991a56488a6d3ebfbef52596
Utkarsh Jaulkar <84310520+utkarshjaulkar@users.noreply.github.com>
2023-01-06 14:39:35 +0530

    Solved 15th question

---
+++ Microsoft/15.java
@@ -0,0 +1,16 @@
+// Substrings containing all three Characters
+// https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/
+
+
+class Solution {
+        public int numberOfSubstrings(String s) {
+        int count[] = {0, 0, 0}, res = 0 , i = 0, n = s.length();
+        for (int j = 0; j < n; ++j) {
+            ++count[s.charAt(j) - 'a'];
+            while (count[0] > 0 && count[1] > 0 && count[2] > 0)
+                --count[s.charAt(i++) - 'a'];
+            res += i;
+        }
+        return res;
+    }
+}
