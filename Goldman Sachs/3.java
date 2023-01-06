f9e5eaacefaa948affcbd8cb00c48224b8e4c265
Utkarsh Jaulkar <84310520+utkarshjaulkar@users.noreply.github.com>
2023-01-06 15:23:04 +0530

    Solved 3rd question

---
+++ Goldman Sachs/3.java
@@ -0,0 +1,18 @@
+// Factorial with Trailing Zeroes
+// https://leetcode.com/problems/factorial-trailing-zeroes/
+
+class Solution {
+    public int trailingZeroes(int n) {
+        
+        int sum=0;
+        while(n>0)
+        {
+            n=n/5;
+            sum = sum + n;
+         
+        }
+        return sum;
+
+    }
+    
+}
